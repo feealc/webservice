5b6d19470a76f7b42ea35f87c2cbe811d450a482
Fernando Alcantara <FernandoBrum@MacBook-Pro-de-Fernando.local>
2016-11-28 22:51:16 -0200

    add first route

--- sources/app.js
+++ sources/app.js
@@ -3,4 +3,15 @@ import express from 'express'
 const server = express()
 const port = process.env.PORT || 3000
 
+server.get('/users', function (req, res) {
+  // res.send('ok')
+  // res.status(204).send('ok')
+  const users = [
+    {name: 'Darlan'},
+    {name: 'Carla'},
+  ]
+  res.json(users)
+})
+
 server.listen(port, () => console.log(`localhost:${port}`))
+
